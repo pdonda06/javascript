408f3b5aac09a964ce55f2132cb36883760e870f
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2024-02-08 00:52:13 +0530

    Add files via upload

---
+++ 04/script.js
@@ -0,0 +1,40 @@
+const age = 23
+
+// if(age >= 80){
+//     console.log("You are adult");
+// }
+// else if(age >=18){
+//     console.log("You can vote");
+// }
+// else{
+//     console.log("No you cant vote");
+// }
+
+// let res = age >=18 ? "Yes" : "No"
+// console.log(res);
+
+// const op = 1
+// switch (op) {
+//     case 0:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//   }
+
+//   console.log(day);
