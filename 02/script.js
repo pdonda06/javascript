fee226e88f47dc86daaac8097e11a0abf93dbb14
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2024-02-07 23:47:31 +0530

    Add files via upload

---
+++ 02/script.js
@@ -0,0 +1,20 @@
+// {
+//     var age =10
+//     console.log(age);
+// }
+// age = 11
+// console.log(age);
+// global, re-assign, kahi bhi access
+
+
+// let age = 29
+// {
+//     let age = 11
+//     console.log(age);
+// }
+// console.log(age);
+// // local, re-assign, potana block ma j access
+
+const age = 11
+console.log(age);
+// local, re-assign no, potana block ma j access
---
+++ 03/script.js
@@ -0,0 +1,47 @@
+// data types
+//numbers
+
+let num =9
+let num2=10
+console.log(num + num2);
+
+//strings
+const fname = "Prince"
+const lname = "Donda"
+
+console.log(fname + ' ' +lname);
+
+//Bolleans 1->true | 0->false
+let isLoggedIn = true
+let isLoggedIn1 = false
+console.log(isLoggedIn);
+
+console.log(isLoggedIn + 10); // 1 + 10
+console.log(isLoggedIn1 + 10); // 1 + 10
+
+//null
+let lastLoginDate = null;
+let lastLoginDate1 = undefined;
+console.log(lastLoginDate);
+console.log(lastLoginDate1);
+
+//objects
+const person = {
+    firstName: 'Prince',
+    lastName: 'Donda',
+    age: 19,
+    isLoggedIn: false,
+}
+console.log(person);
+console.log(person.age);
+
+// strings '1' + '1'
+// s + n = s
+// s * n = n
+console.log('1' + '1');
+console.log('123' * 1); //string to number 
+console.log('a' * 1); 
+
+// typeof operator
+console.log(typeof('123' * 1));
+console.log(typeof fname);
