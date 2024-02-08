bd72735d5a2e9c398385add4cb5e819b80095c1c
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2024-02-09 00:22:56 +0530

    Add files via upload

---
+++ 05/script.js
@@ -0,0 +1,24 @@
+const age=9
+const gender="male"
+
+if(age>=18 && gender=="male"){
+    console.log("Yes, you are good to go");
+}
+else{
+    console.log("Stop now");
+}
+
+
+if(age>=18 || gender=="male"){
+    console.log("Yes, you are good to go");
+}
+else{
+    console.log("Stop now");
+}
+
+if(age%2 != 0){
+    console.log("hh");
+}
+else{
+    console.log("nn");
+}
